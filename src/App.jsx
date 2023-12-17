a8939c10054a6eaa3a132ce6138f8cb91a6f0a40
--global <--global>
2023-12-17 02:16:41 +0200

    Added routes in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return (
-    <div className='text-blue-500'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-out" element={<SignOut/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+
+  </Routes>
+  </BrowserRouter>
 }
